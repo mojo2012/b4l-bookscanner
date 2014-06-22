70af11978eb78e1ce262d8c316d3c0919300abca
meister.fuchs@gmail.com <meister.fuchs@gmail.com@cc17ed2a-df65-d4d3-f706-802ad2cc21e6>
2014-06-22 19:54:41 +0000

    updated for books4life app

--- B4LBookScanner/src/at/spot/b4lbookscanner/activity/BookDetailsActivity.java
+++ B4LBookScanner/src/at/spot/b4lbookscanner/activity/BookDetailsActivity.java
@@ -1,8 +1,10 @@
 package at.spot.b4lbookscanner.activity;
 
+import java.net.MalformedURLException;
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 
@@ -28,6 +30,10 @@ import at.spot.b4lbookscanner.googlebooks.VolumeInfo;
 import at.spot.b4lbookscanner.googlebooks.VolumeList;
 import at.spot.b4lbookscanner.persistence.DatabaseHandler;
 import at.spot.b4lbookscanner.util.Util;
+import at.spot.prestashop.service.PrestaShopClient;
+import at.spot.prestashop.service.json.EStatus;
+import at.spot.prestashop.service.json.Product;
+import at.spot.util.HttpUtil;
 
 public class BookDetailsActivity extends Activity {
 	TextView						title			= null;
@@ -77,9 +83,13 @@ public class BookDetailsActivity extends Activity {
 		saveButton.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				showPriceSelectionDialog();
+				saveBook();
 			}
 		});
+	}
+	
+	private void saveBook() {
+		showPriceSelectionDialog();
 	}
 
 	private void showPriceSelectionDialog() {
@@ -98,7 +108,14 @@ public class BookDetailsActivity extends Activity {
 						book.setPrice(prices[item]);
 
 						dialog.dismiss();
-						setBookNumber();
+//						setBookNumber();
+						
+						try {
+							saveBook(book);
+						} catch (Exception e) {
+							
+							e.printStackTrace();
+						}
 					}
 				});
 
@@ -141,12 +158,18 @@ public class BookDetailsActivity extends Activity {
 
 				Book b = db.getBookByTitle(book.getTitle());
 
-				if (b != null) {
-					book.setId(b.getId());
-
-					db.updateBook(book);
-				} else {
-					db.addBook(book);
+//				if (b != null) {
+//					book.setId(b.getId());
+//
+//					db.updateBook(book);
+//				} else {
+//					db.addBook(book);
+//				}
+				
+				try {
+					saveBook(b);	
+				} catch (Exception e) {
+					e.printStackTrace();
 				}
 
 				dialog.dismiss();
@@ -157,6 +180,82 @@ public class BookDetailsActivity extends Activity {
 		AlertDialog d = inputDialog.create();
 		d.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
 		d.show();
+	}
+	
+	private void saveBook(Book book) throws Exception {
+		try {
+			PrestaShopClient pc = new PrestaShopClient("http://shop.b4l-wien.at/service", "NL0VF0CJ48EE3QVNHO4W0ZWCZ6UY1VPN");
+			
+			Product p = new Product();
+			p.setTitle(book.getTitle());
+			p.setAmount(1);
+			p.setShortDescription(getSummary(book));
+			p.setLongDescription(getSummary(book));
+			p.setPrice(book.getPrice());
+			p.setTags(getTags(book));
+			p.setCategoryId(33);
+			p.setOnline(false);
+			p.setStatus(EStatus.Used);
+//			p.setSupplierId(32);
+			p.setStockLocationId(36);
+			p.setVatRate(10f);
+			p.setEan13(book.getIsbn());
+			
+			try {
+				byte[] image = null;//Files.readAllBytes(Paths.get("/Users/matthias/Desktop/913.jpg"));
+				
+				if (book.getImageUrl() != null) {
+					image = HttpUtil.download(book.getImageUrl());
+				}
+				
+				if (image != null) {
+					p.getImages().add(image);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+			pc.addProduct(p);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}	
+	}
+	
+	private List<String> getTags(Book book) {
+		List<String> tags = new ArrayList<String>();
+		
+		if (book.getAuthors() != null)
+			tags.add(book.getAuthors());
+		
+		if (book.getPublisher() != null)
+			tags.add(book.getPublisher());
+		
+		if (book.getTitle() != null)
+			tags.add(book.getTitle());
+		
+		return tags;
+	}
+	
+	private String getSummary(Book book) {
+		String ret = "";
+		
+		if (book.getSummary() != null) {
+			ret = book.getSummary();
+		} else {
+			if (book.getAuthors() != null) {
+				ret += "\nAuthors: " + book.getAuthors();
+			}
+			
+			if (book.getPublisher() != null) {
+				ret += "\nPublisher: " + book.getPublisher();
+			}
+			
+			if (book.getReleaseDate() != null) {
+				ret += "\nRelease date: " + book.getReleaseDate();
+			}
+		}
+		
+		return ret.trim();
 	}
 
 	private void fillDetails() {
