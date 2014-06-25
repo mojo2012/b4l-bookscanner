dc4537d07e60f824414e5a312bc356b91f16121a
meister.fuchs@gmail.com <meister.fuchs@gmail.com@cc17ed2a-df65-d4d3-f706-802ad2cc21e6>
2014-06-25 17:21:31 +0000

    added datastore

---
+++ B4LBookScanner/src/at/spot/b4lbookscanner/persistence/DataStore.java
@@ -0,0 +1,10 @@
+package at.spot.b4lbookscanner.persistence;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import at.spot.prestashop.service.json.Category;
+
+public class DataStore {
+	public static List<Category>	categories	= new ArrayList<Category>();
+}
